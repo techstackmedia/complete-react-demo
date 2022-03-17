8bcecd1d12183b8056f1232866ac8ae79e95cf7f
charlesOnuorah <charles.onuorah@yahoo.com>
2022-03-17 12:21:46 +0100

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,3 @@
-// const Pet = () => {
-//     return React.createElement("div", {}, [
-//       React.createElement("h1", {}, "Luna"),
-//       React.createElement("h2", {}, "Dog"),
-//       React.createElement("h2", {}, "Havanese"),
-//     ]);
-//   };
-  
-//   const App = () => {
-//     return React.createElement("div", {}, [
-//       React.createElement("h1", {}, "Adopt Me!"),
-//       React.createElement(Pet),
-//       React.createElement(Pet),
-//       React.createElement(Pet),
-//     ]);
-//   };
-  
-//   ReactDOM.render(React.createElement(App), document.getElementById("root"));
-
-
-// The one way data flow
 const Pet = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
@@ -45,28 +24,3 @@ const App = () => {
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
-
-/*
-<body>
-    <div id="root">
-    <div>
-        <h1>Adopt Me!</h1>
-        <div>
-            <h1>Luna</h1>
-            <h2>Dog</h2>
-            <h2>Havanese</h2>
-        </div>
-        <div>
-            <h1>Pepper</h1>
-            <h2>Bird</h2>
-            <h2>Cockatiel</h2>
-        </div>
-        <div>
-            <h1>Doink</h1>
-            <h2>Cat</h2>
-            <h2>Mix</h2>
-        </div>
-    </div>
-</div>
-</body>
-*/
